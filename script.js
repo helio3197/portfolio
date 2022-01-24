df33c4dfc54eb1543ddf82b72acff7da0e8ddc29
Kenny <kennysalazar3197@gmail.com>
2022-01-24 17:58:52 -0500

    Fixed bugs on the mobile menu

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 const header = document.getElementById('header');
-const logo = document.getElementById('logo')
+const logo = document.getElementById('logo');
 const menu_icon = document.getElementById('menu-icon');
 const close_menu = document.getElementById('close-menu');
 const navbar = document.getElementById('navbar');
-const mail_icon = document.getElementById('mail-icon')
-const mobile_nav = document.getElementById('mobile-nav')
+const mail_icon = document.getElementById('mail-icon');
+const mobile_nav = document.getElementById('mobile-nav');
 
 function openMenu() {
   header.classList.add('mobile-menu');
   logo.style.display = "none";
-  menu_icon.style.display = "none";
+  menu_icon.classList.replace("menu-icon", "menu-icon-close"); 
   close_menu.style.display = "block";
-  navbar.style.display = "block";
-  mail_icon.style.display = "none"
+  navbar.classList.replace("navbar","navbar-open");
   mobile_nav.classList.add('mobile-menu-nav')
 }
 
 function closeMenu() {
   header.classList.remove('mobile-menu');
   logo.style.display = "block";
-  menu_icon.style.display = "block";
+  menu_icon.classList.replace("menu-icon-close", "menu-icon");
   close_menu.style.display = "none";
-  navbar.style.display = "none";
-  mail_icon.style.display = "none"
+  navbar.classList.replace("navbar-open", "navbar");
   mobile_nav.classList.remove('mobile-menu-nav') 
 
 }
