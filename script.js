0cb63b2d7b8da9c515d03bb36917147b3cefa94c
Kenny <kennysalazar3197@gmail.com>
2022-01-24 23:33:45 -0500

    Added animations to the mobile menu

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
 const header = document.getElementById('header');
 const logo = document.getElementById('logo');
-const menu_icon = document.getElementById('menu-icon');
-const close_menu = document.getElementById('close-menu');
+const menuIcon = document.getElementById('menu-icon');
+const closeMenuIcon = document.getElementById('close-menu');
 const navbar = document.getElementById('navbar');
-const mail_icon = document.getElementById('mail-icon');
-const mobile_nav = document.getElementById('mobile-nav');
+const mobileNav = document.getElementById('mobile-nav');
+const menuElement = document.getElementsByClassName('menu-option');
+const navbarLi = document.querySelectorAll('#navbar li');
 
 function openMenu() {
-  header.classList.add('mobile-menu');
-  logo.style.display = "none";
-  menu_icon.classList.replace("menu-icon", "menu-icon-close"); 
-  close_menu.style.display = "block";
-  navbar.classList.replace("navbar","navbar-open");
-  mobile_nav.classList.add('mobile-menu-nav')
+  header.classList.add('mobile-menu', 'mobile-menu-animation');
+  logo.style.display = 'none';
+  menuIcon.classList.replace('menu-icon', 'menu-icon-close');
+  closeMenuIcon.style.display = 'block';
+  navbar.classList.replace('navbar', 'navbar-open');
+  mobileNav.classList.add('mobile-menu-nav');
+  setTimeout(() => {
+    for (let i = 0; i < 3; i += 1) {
+      menuElement[i].classList.add('menu-animation');
+      navbarLi[i].classList.add('navbar-animation');
+    }
+  }, 1);
 }
 
 function closeMenu() {
-  header.classList.remove('mobile-menu');
-  logo.style.display = "block";
-  menu_icon.classList.replace("menu-icon-close", "menu-icon");
-  close_menu.style.display = "none";
-  navbar.classList.replace("navbar-open", "navbar");
-  mobile_nav.classList.remove('mobile-menu-nav') 
-
+  for (let i = 0; i < 3; i += 1) {
+    menuElement[i].classList.remove('menu-animation');
+    navbarLi[i].classList.remove('navbar-animation');
+  }
+  header.classList.remove('mobile-menu-animation');
+  closeMenuIcon.style.display = 'none';
+  setTimeout(() => {
+    logo.style.display = 'block';
+    menuIcon.classList.replace('menu-icon-close', 'menu-icon');
+    navbar.classList.replace('navbar-open', 'navbar');
+    mobileNav.classList.remove('mobile-menu-nav');
+    header.classList.remove('mobile-menu');
+  }, 350);
 }
 
-menu_icon.addEventListener('click', openMenu, true);
-close_menu.addEventListener('click', closeMenu, true);
+menuIcon.addEventListener('click', openMenu, true);
+closeMenuIcon.addEventListener('click', closeMenu, true);
+menuElement[0].addEventListener('click', closeMenu, true);
+menuElement[1].addEventListener('click', closeMenu, true);
+menuElement[2].addEventListener('click', closeMenu, true);
